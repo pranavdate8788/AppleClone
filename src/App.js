ab2b26316a847a24167e3ca2884e79b82b6d3dd8
pranav date <pranavdate908@gmail.com>
2024-05-17 08:34:27 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+ 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AppNavbar from './Components/AppNavbar';
+import Store from './Components/Store';
+import Home from './Components/Home';
+// import Mac from './Components/Mac';
+// import Ipad from './Components/Ipad';
+// import Watch from './Components/Watch';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppNavbar />
+      <div className="container-fluid">
+        <Routes>
+         <Route path="/store" element={<Store />} />  
+         <Route path="/" element={<Home />} />  
+
+          {/* <Route path="/mac" element={<Mac />} />
+          <Route path="/ipad" element={<Ipad />} />
+          <Route path="/watch" element={<Watch />} />
+     
+          {/* <Route path="/" element={<Store />} /> */} 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
